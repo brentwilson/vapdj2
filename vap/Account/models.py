41fab2442fb7053f256aa37026c48522e115fcf7
brentwilson <brent@freshguava.com>
2025-01-10 12:09:41 -0800

    add acount model

---
+++ vap/Account/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class UserAccount(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Company(models.Model):
+    company_id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+      ordering = ['name']
+      verbose_name_plural = 'Companies'
+    
+    def __str__(self):
+      return self.name
